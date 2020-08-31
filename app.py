91a666fcb90a2866978f75cec6c731645d6e2d4b
hyunjin <hyunjin0723@naver.com>
2020-08-31 18:47:11 +0900

    carousel append가 안 됨
    img와 url까지는 잘 가져왔는데 carousel에 append하니까 한 칸에 세로로 다 나옴

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, jsonify
 import twitter
 import jsonpickle
 import requests
+import json
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.post('https://now.smtown.com/news/more',headers=headers,data={'pageSize': 50})
-soup = BeautifulSoup(data.text, 'html.parser')
+# data = requests.post('https://now.smtown.com/news/more',headers=headers,data={'pageSize': 50})
+# soup = BeautifulSoup(data.text, 'html.parser')
 
 # import json
 # class Object:
@@ -62,30 +63,70 @@ def taemin():
 def bookmark():
     return render_template('bookmark.html')
 
-########## Home page
+######### Home page
+@app.route('/youtube', methods=['GET'])
+def youtube():
+    youtube_data = requests.get('https://www.youtube.com/playlist?list=UUyPwRgc3gQGqhk6RoGS50Ug', headers=headers)
+    soup = BeautifulSoup(youtube_data.text, 'html.parser') #BeautifulSoup로 Response 값을 분석합니다.
+    scripts = soup.find_all('script') # <script> 태그가 있는 부분만 찾아내어 Set으로 반환합니다.
+
+    found_i = -1
+    for (i, x) in enumerate(scripts):
+        if 'window["ytInitialData"] = ' in str(x):  # ytInitialData가 담긴 객체를 검색합니다.
+            found_i = i
+            break
+    if found_i < 0:
+        print('Cannot find playlist')  # 만일 ytInitialData가 담긴 객체를 찾지 못했다면 그냥 종료합니다.
+        exit()
+
+    data = scripts[found_i]
+    str1 = data.string.strip().split('window["ytInitialData"] = ')[1].split(';\n')[0]
+    video = json.loads(str1, encoding='utf8', strict=False)  # String을 JSON Parsing하여 파이썬에서 사용할 수 있도록 해줍니다.
+        # 이 때 strict를 false로 하지 않으면 인코딩 문제로 loads()가 제대로 실행되지 않을 수 있습니다.
+    video_ids = \
+    video['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer'][
+        'contents'][0]['itemSectionRenderer']['contents'][0]['playlistVideoListRenderer']['contents']
+    youtubes = []
+    for video_id in video_ids:
+        url = 'https://www.youtube.com/watch?v=' + video_id['playlistVideoRenderer']['videoId']
+        img = video_id['playlistVideoRenderer']['thumbnail']['thumbnails'][0]['url']
+        origin_img = img.split('?')[0]
+        youtube = {'url': url, 'origin_img': origin_img}
+        youtubes.append(youtube)
+    return jsonify({'result': 'success', 'youtubes': youtubes})
+
+
 @app.route('/smtown', methods=['GET'])
 def smtown():
+    smtown_data = requests.post('https://now.smtown.com/news/more', headers=headers, data={'pageSize': 50})
+    soup = BeautifulSoup(smtown_data.text, 'html.parser')
     smtowns = soup.select('li.grid-item')
-    data = []
+    smnews = []
     for smtown in smtowns:
-        doc = {headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip(),
-        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip(),
-        time = smtown.select_one('div > div.contentInfo > ul > li').text,
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
         img = smtown.select_one('div > div.thumb > a > img')
         if img is None:
             img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
         else:
-            img = img['src'],
+            img = img['src']
         url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
         if 'youtube' not in url:
-            url = 'https://now.smtown.com' + url}
-        data.append(doc)
-    return jsonify({'result': 'success', 'headlines': headline, 'contents': content, 'times': time, 'imgs': img, 'urls': url})
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
 
 @app.route('/twitter', methods=['GET'])
 def twitter():
     query = "샤이니"
-    twitters = twitter_api.GetSearch(term=query, count=30, lang='ko')
+    twitters = twitter_api.GetSearch(term=query, count=50, lang='ko')
     # return json.dumps({'result': 'success', 'twitters': twitters}, default=dumper, indent=2)
     return jsonpickle.encode({'result': 'success', 'twitters': twitters})
 
@@ -104,6 +145,31 @@ def navernews():
     return jsonpickle.encode({'result': 'success', 'navernews': navernews})
 
 ########## onew page
+@app.route('/smtown_onew', methods=['GET'])
+def smtown_onew():
+    smtowns = soup.select('li.grid-item')
+    smnews = []
+    for smtown in smtowns:
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
+        img = smtown.select_one('div > div.thumb > a > img')
+        if img is None:
+            img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
+        else:
+            img = img['src']
+        url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
+        if 'youtube' not in url:
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
+
 @app.route('/twitter_onew', methods=['GET'])
 def twitter_onew():
     query = "온유"
@@ -126,6 +192,31 @@ def navernews_onew():
     return jsonpickle.encode({'result': 'success', 'navernews': navernews})
 
 ########## jonghyun page
+@app.route('/smtown_jonghyun', methods=['GET'])
+def smtown_jonghyun():
+    smtowns = soup.select('li.grid-item')
+    smnews = []
+    for smtown in smtowns:
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
+        img = smtown.select_one('div > div.thumb > a > img')
+        if img is None:
+            img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
+        else:
+            img = img['src']
+        url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
+        if 'youtube' not in url:
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
+
 @app.route('/twitter_jonghyun', methods=['GET'])
 def twitter_jonghyun():
     query = "샤이니 종현"
@@ -149,6 +240,31 @@ def navernews_jonghyun():
 
 
 ########## key page
+@app.route('/smtown_key', methods=['GET'])
+def smtown_key():
+    smtowns = soup.select('li.grid-item')
+    smnews = []
+    for smtown in smtowns:
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
+        img = smtown.select_one('div > div.thumb > a > img')
+        if img is None:
+            img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
+        else:
+            img = img['src']
+        url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
+        if 'youtube' not in url:
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
+
 @app.route('/twitter_key', methods=['GET'])
 def twitter_key():
     query = "샤이니 키"
@@ -172,6 +288,31 @@ def navernews_key():
 
 
 ########## minho page
+@app.route('/smtown_minho', methods=['GET'])
+def smtown_minho():
+    smtowns = soup.select('li.grid-item')
+    smnews = []
+    for smtown in smtowns:
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
+        img = smtown.select_one('div > div.thumb > a > img')
+        if img is None:
+            img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
+        else:
+            img = img['src']
+        url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
+        if 'youtube' not in url:
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
+
 @app.route('/twitter_minho', methods=['GET'])
 def twitter_minho():
     query = "샤이니 민호"
@@ -195,6 +336,31 @@ def navernews_minho():
 
 
 ########## taemin page
+@app.route('/smtown_taemin', methods=['GET'])
+def smtown_taemin():
+    smtowns = soup.select('li.grid-item')
+    smnews = []
+    for smtown in smtowns:
+        headline = smtown.select_one('div > div.contentInfo > p.title.text_box > a').text.strip()
+        content = smtown.select_one('div > div.contentInfo > p.subInfo.text_box > a').text.strip()
+        time = smtown.select_one('div > div.contentInfo > ul > li').text
+        img = smtown.select_one('div > div.thumb > a > img')
+        if img is None:
+            img = smtown.select_one('div > div.thumb > a')['style'].split("'")[1]
+        else:
+            img = img['src']
+        url = smtown.select_one('div > div.contentInfo > p.title.text_box > a')['href']
+        if 'youtube' not in url:
+            url = 'https://now.smtown.com' + url
+        sm = {
+            'headline': headline,
+            'content': content,
+            'time': time,
+            'img': img,
+            'url': url}
+        smnews.append(sm)
+    return jsonify({'result': 'success', 'smnews': smnews})
+
 @app.route('/twitter_taemin', methods=['GET'])
 def twitter_taemin():
     query = "태민"
